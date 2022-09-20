2f46b16ef403239f1acad96ddd5864c7e2c24de8
barrakz <b.rakuzy@gmail.com>
2022-09-20 19:59:02 +0200

    app file coding

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+
+api_key = '3e77de4b98cfb3b5c5e7ed7af9b06bc7'
+user_input = input("Enter city: ")
+print(user_input)
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+weather = weather_data.json()['weather'][0]['main']
+temp = round(weather_data.json()['main']['temp'])
+
+print(f"The weather in {user_input} is {weather}")
+print(f"The temperature in {user_input} is {temp}F")
+# print(weather_data.status_code)
