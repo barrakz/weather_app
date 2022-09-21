3ea74b97e3e328babe388008a88739cf25322c99
barrakz <b.rakuzy@gmail.com>
2022-09-21 16:30:26 +0200

    update app file with celsius converter

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import requests
 
+from celsius_converter import celsius_converter
+
 api_key = '3e77de4b98cfb3b5c5e7ed7af9b06bc7'
 user_input = input("Enter city: ")
 print(user_input)
@@ -9,8 +11,11 @@ weather_data = requests.get(
 
 weather = weather_data.json()['weather'][0]['main']
 temp = round(weather_data.json()['main']['temp'])
+celsius_temp = celsius_converter(temp)
 
 print(f"The weather in {user_input} is {weather}")
-print(f"The temperature in {user_input} is {temp}F")
+print(f"The temperature in {user_input} is {celsius_temp} C")
 # print(weather_data.status_code)
+
+
 
